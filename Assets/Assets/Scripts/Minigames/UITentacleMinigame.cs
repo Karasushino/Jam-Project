77a704ed3d86e9f1596c1d0f27cf2e4a27577739
Daniel Martinez <adlerdurden@gmail.com>
2020-10-13 19:14:16 +0100

    TentacleSecondPush

--- Assets/Assets/Scripts/Minigames/UITentacleMinigame.cs
+++ Assets/Assets/Scripts/Minigames/UITentacleMinigame.cs
@@ -8,8 +8,8 @@ public class UITentacleMinigame : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject[] Tools;
-    
-  //  public 
+
+    private bool bHaveTool;
 
     public Slider flaskBar;
 
@@ -21,16 +21,19 @@ public class UITentacleMinigame : MonoBehaviour
     private float maxPorcentage = 100.0f;
 
     private float percentageOfTempBar = 0.0f;
-    
-    
+
+    public int iRightTool = 2;
+    private int iSelectedTool = 10;
     void Start()
     {
-        
+
+        bHaveTool = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateFlaskBar();
         
     }
     
@@ -50,14 +53,38 @@ public class UITentacleMinigame : MonoBehaviour
 
     }
 
-    void FillFlask()
+    public void SelectTool(int i)
     {
-        //Add to current temperature
-        currentPorcentage += porcToAddPerClick;
+        bHaveTool = true;
+        iSelectedTool = i;
+        for (int a = 0; a < Tools.Length; a++)
+        {
+            Tools[a].GetComponent<Button>().interactable = false;
+        }
+    }
 
-        //If we added more temperature than we should have set it back to max
-        if (currentPorcentage > maxPorcentage)
-            currentPorcentage = maxPorcentage; 
+    public void RestartMinigame()
+    {
+        for (int a = 0; a < Tools.Length; a++)
+        {
+            Tools[a].GetComponent<Button>().interactable = true;
+        }
+        iSelectedTool = 10;
+        bHaveTool = false;
+        currentPorcentage = 1;
+    }
+    
+   public void FillFlask()
+    {
+        if (bHaveTool && currentPorcentage < objectivePorcentage)
+        {
+            //Add to current temperature
+            currentPorcentage += porcToAddPerClick;
+
+            //If we added more temperature than we should have set it back to max
+            if (currentPorcentage > maxPorcentage)
+                currentPorcentage = maxPorcentage; 
+        }
     }
     
 }
